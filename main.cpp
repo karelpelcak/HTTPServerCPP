21d33b1cfea01474909af48ca6470f0c5c5200f4
Pupa's dev <kaja.pelcak@seznam.cz>
2024-03-27 11:09:33 +0100

    clear comments

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include "agrManage.hpp"
 #include <iostream>
-#include <string> // Pro práci s řetězci
+#include <string>
 
 int main(int argc, char *argv[]) {
-    // Zkontrolujte, zda byl zadán argument --port a je následován číslem portu
     int port = ArgManager::getPort(argc, argv);
     if (port == -1) {
         std::cerr << "Chyba: očekává se právě 1 argument --port=cisloportu" << std::endl;
         return 1;
     }
 
-    // Zkontrolujte, zda je přítomen argument --dev
     bool devOption = ArgManager::hasDevArgument(argc, argv);
 
     std::cout << "Port: " << port << std::endl;
